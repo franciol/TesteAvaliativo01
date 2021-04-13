85d259eb71d31ef7ed8d8b381a0f7c9930d57741
Francisco Ciol Rodrigues Aveiro <fran.aveiro@hotmail.com>
2021-04-13 16:35:56 -0300

    Ajuste no  espaco e do README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Escreva um algoritmo para que se um elemento em uma matriz MxN é 0, todos os el
 
 Exemplo:
 
-![Alt text](Imagem/imagem1.png?raw=true "Exemplo")
+![Exemplo](Imagem/imagem1.png?raw=true "Exemplo")
 
 O código deve avaliar qualquer possível “zero” de uma matriz de input, não só o primeiro.
 
@@ -13,4 +13,4 @@ Essa solução demonstrou as seguintes complexidades:
 
 Complexidade de tempo: O(MxN)
 
-Complexidade de Espaço: O(MxN)
+Complexidade de Espaço: O(M+N)
--- teste_matriz.py
+++ teste_matriz.py
@@ -24,7 +24,7 @@ matriz_exemplo_solucao = [
 ''' Função converte_zero recebe uma matriz(lista de listas) e retorna uma matriz com zero em todas as linhas e colunas que tenham zero na matriz de entrada. 
     A complexidade dessa solução se dá da seguinte forma:
         Tempo:      O(m * n) + O(m * n) + O(m * n) =    O(mn)
-        Espaço:     O(m * n) + O(m + n)             =   O(mn)
+        Espaço:     O(m + n)                       =    O(m+n) ** Sem considerar otamanho da matriz de entrada **
  '''
 def converte_zero(matriz):
     dict_coords = {'x':set(),'y':set()}                                         # complexidade de espaco:   O(m+n)
@@ -34,15 +34,13 @@ def converte_zero(matriz):
                 dict_coords['x'].add(index_x)                                   # complexidade de tempo:    O(1)
                 dict_coords['y'].add(index_y)                                   # complexidade de tempo:    O(1)    portanto O(mn) no primeiro for()
                                                                                 
-    res = matriz.copy()                                                         # complexidade de Espaco:   O(mn)
-                                                                                # complexidade de tempo:    O(mn)   no copy()
 
-    for  index_y, value_y in enumerate(res):                                    # complexidade de tempo:    O(m)
+    for  index_y, value_y in enumerate(matriz):                                    # complexidade de tempo:    O(m)
         for index_x, value_x in enumerate(value_y):                             # complexidade de tempo:    O(n)
             if(index_x in dict_coords['x'] or index_y in dict_coords['y']):     # complexidade de tempo:    O(1)
-                res[index_y][index_x] = 0                                       # complexidade de tempo:    O(1)    portanto O(mn) no segundo for()
+                matriz[index_y][index_x] = 0                                       # complexidade de tempo:    O(1)    portanto O(mn) no segundo for()
 
-    return res
+    return matriz
 
 # Aqui comeca a main
 res = converte_zero(matriz_exemplo)
